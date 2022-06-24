2d3a5ea0156b8c8154b7fa4c99081701f3c7115e
Reza Nazari <rz.nzrisg@gmail.com>
2022-06-24 18:17:22 +0430

    doc: write major documentations for calculations stuffs

--- utilities/useGameLogic/index.ts
+++ utilities/useGameLogic/index.ts
@@ -3,6 +3,7 @@ import { Position } from '../useGridCalculation/types';
 import { initialMoves, initialTurn } from './constants';
 import { GameStateItem, Player, PlayersContainer } from './types';
 
+// custom hooks which handles all of the game logics like moves, winners, etc
 export const useGameLogic = ({
   size,
   onFinish,
@@ -11,7 +12,11 @@ export const useGameLogic = ({
   onFinish: (e: { winner?: Player; reset: () => void }) => void;
 }) => {
   const [turn, setTurn] = useState<Player>(initialTurn);
+
+  // this state will be used for rendering a grid.
   const [state, setState] = useState<GameStateItem[]>([]);
+
+  // to achieve better performance and time complexity we use the static containers doing the calculations
   const movesCount = useRef<number>(0);
   const movesContainer = useRef<PlayersContainer>(initialMoves(size));
 
@@ -35,12 +40,17 @@ export const useGameLogic = ({
 
   const updatePlayerMove = useCallback(
     (position: Position) => {
+      // we've got four static containers for every player.
+      // every container will be increased by 1 based on move type (row, column, diagonal, opposite diagonal)
+
       movesContainer.current[turn].rows[position.row] += 1;
       movesContainer.current[turn].columns[position.column] += 1;
 
+      // check for diagonal move
       if (position.row === position.column)
         movesContainer.current[turn].diagonal += 1;
 
+      // check for opposite diagonal move
       if (position.row + position.column === size - 1)
         movesContainer.current[turn].oppositeDiagonal += 1;
     },
@@ -49,6 +59,8 @@ export const useGameLogic = ({
 
   const checkForWinner = useCallback(
     (position: Position) => {
+      // when one of the container reaches to grid size the user will be considered as a winner
+
       const winByRow = movesContainer.current[turn].rows[position.row] === size;
       const winByColumn =
         movesContainer.current[turn].columns[position.column] === size;
@@ -65,6 +77,8 @@ export const useGameLogic = ({
   );
 
   const checkForFinished = useCallback(() => {
+    // when you've done with moves based on grid size the game will be finished without any winners
+
     const isFinished = movesCount.current === size * size;
     return isFinished;
   }, [size]);
--- utilities/useGridCalculation/index.ts
+++ utilities/useGridCalculation/index.ts
@@ -1,6 +1,7 @@
 import { DependencyList, useCallback, useMemo } from 'react';
 import { CheckIfOnBorder, GetLocation, GetPosition } from './types';
 
+// custom hooks which is responsible to do all the grid's calculations
 export const useGridCalculation = ({
   size,
   borderSize,
@@ -10,15 +11,21 @@ export const useGridCalculation = ({
   borderSize: number;
   space: number;
 }) => {
+  // we should ensure the calculations will be regenerated only once the props are changed
   const deps: DependencyList = [size, borderSize, space];
 
+  // the border count based on the grid size
   const borderCount = useMemo<number>(() => size - 1, deps);
 
+  // the item size will be calculated by the space and grid size
+  // note that we've considered the borders size and they're subtracted from grid space
   const itemSize = useMemo<number>(
     () => (space - borderCount * borderSize) / size,
     deps
   );
 
+  // to render the border we're gonna calculate the start position of each border inside the grid
+  // note that the calculations of a border is dependent to previous borders and items
   const bordersStartPosition = useMemo<number[]>(
     () =>
       Array.from({ length: borderCount }).map(
@@ -27,17 +34,21 @@ export const useGridCalculation = ({
     deps
   );
 
+  // check if the touched location is on the borders. we use this method to skip the move event when the user touches on the borders
   const checkIfOnBorder = useCallback<CheckIfOnBorder>(({ x, y }) => {
     return bordersStartPosition.some(
       (p) => (x >= p && x <= p + borderSize) || (y >= p && y <= p + borderSize)
     );
   }, deps);
 
+  // get the grid's row and column based on absolute position
   const getPosition = useCallback<GetPosition>((location) => {
     const isOnBorder = checkIfOnBorder(location);
     if (isOnBorder)
       return { position: { row: -1, column: -1 }, isOnBorder: true };
 
+    // sum of the borders size which have got smaller location than current
+    // we'll need both of vertical and horizontal borders
     const verticalBordersSize =
       bordersStartPosition.filter((p) => p < location.x).length * borderSize;
     const horizontalBordersSize =
@@ -48,6 +59,7 @@ export const useGridCalculation = ({
     return { position: { row, column }, isOnBorder: false };
   }, deps);
 
+  // get the absolute position based on the row and column
   const getLocation = useCallback<GetLocation>(({ row, column }) => {
     const locationX = column * itemSize + column * borderSize;
     const locationY = row * itemSize + row * borderSize;
